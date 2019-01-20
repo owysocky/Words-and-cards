e2e0c0d9a91ef0542e933069c8f02572a3ce344b
ow <volovykolya@gmail.com>
2019-01-20 12:44:22 -0800

    add card stack

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,6 @@ $(document).ready(function() {
 
   $("#formOne").submit(function(event){
 
-
-
     var theInput = $("input#sentance").val();
     var words = theInput.split(" ");
     var newWords = [];
@@ -21,6 +19,17 @@ $(document).ready(function() {
     });
 
    $("#add").text(string);
+
+
+   var ranks = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+   var suits = ["clubs","hearts","diamonds","spades"];
+
+   suits.forEach(function(suite){
+     ranks.forEach(function(rank){
+       $("#cards").append("<li>" + rank + " of " + suite + "</li>");
+     });
+   });
+
 
 
 
